21c5fd3895a79bb6459b097aaae63cecf547ef8f
Yak! <yak1ex@users.noreply.github.com>
2026-01-04 16:33:09 +0900

    Complete section 2.4 except for exercise.

--- chapter2/feline/Main.lean
+++ chapter2/feline/Main.lean
@@ -1,2 +1,42 @@
-def main : IO Unit :=
-  IO.println s!"Hello, cats!"
+/- 2.4 Worked Example: cat -/
+def bufsize : USize := 20 * 1024
+
+partial def dump (stream : IO.FS.Stream) : IO Unit := do
+  let buf ← stream.read bufsize
+  if buf.isEmpty then
+    pure ()
+  else
+    let stdout ← IO.getStdout
+    stdout.write buf
+    dump stream
+
+def fileStream (filename : System.FilePath) : IO (Option IO.FS.Stream) := do
+  let fileExists ← filename.pathExists
+  if not fileExists then
+    let stderr ← IO.getStderr
+    stderr.putStrLn s!"File not found: {filename}"
+    pure none
+  else
+    let handle ← IO.FS.Handle.mk filename IO.FS.Mode.read
+    pure (some (IO.FS.Stream.ofHandle handle))
+
+def process (exitCode : UInt32) (args: List String) : IO UInt32 := do
+  match args with
+  | [] => pure exitCode
+  | "-" :: args =>
+    let stdin ← IO.getStdin
+    dump stdin
+    process exitCode args
+  | filename :: args =>
+    let stream ← fileStream (filename)
+    match stream with
+    | none =>
+      process 1 args
+    | some stream =>
+      dump stream
+      process exitCode args
+
+def main (args : List String): IO UInt32 :=
+  match args with
+  | [] => process 0 ["-"]
+  | _ => process 0 args
